ae0afe47af6bd2c59bd3deb1b0c022f6136b2e29
AlfredWeddborn <vargasleda382@gmail.com>
2026-05-16 21:04:04 -0600

    login

---
+++ backend/SmartAccess/SmartAccess.API/Services/AuthService.cs
@@ -0,0 +1,56 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+using SmartAccess.API.Models;
+
+namespace SmartAccess.API.Services
+{
+    public class AuthService
+    {
+        private readonly FirestoreDb _firestoreDb;
+
+        public AuthService()
+        {
+            // Inicializa la conexión con tu archivo secreto de Firebase
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromFile("firebase-config.json")
+                });
+            }
+            // Cambia "smart-access-edge" por el ID exacto de tu proyecto de Firebase
+            _firestoreDb = FirestoreDb.Create("smart-access-edge");
+        }
+
+        public async Task<User?> ValidarTokenFirebaseAsync(string idToken)
+        {
+            try
+            {
+                // Descifra el token que viene desde la interfaz de Angular
+                FirebaseToken decodedToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(idToken);
+                string uid = decodedToken.Uid;
+
+                // Busca en Firestore los privilegios del usuario (Admin o Empleado)
+                DocumentReference docRef = _firestoreDb.Collection("Users").Document(uid);
+                DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+
+                if (!snapshot.Exists) return null;
+
+                return new User
+                {
+                    UserId = uid,
+                    Email = snapshot.GetValue<string>("Email"),
+                    Nombre = snapshot.GetValue<string>("Nombre"),
+                    Rol = snapshot.GetValue<string>("Rol"),
+                    CheckedIn = snapshot.GetValue<bool>("CheckedIn")
+                };
+            }
+            catch
+            {
+                return null; // Token manipulado o vencido
+            }
+        }
+    }
+}
