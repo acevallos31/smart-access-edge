2314c942dca575b90e056a38830878fd3b94fdf1
Axeell Cevallos <acevallos31@gmail.com>
2026-06-15 12:54:14 -0600

    CI/CD

--- backend/SmartAccess/SmartAccess.API/Program.cs
+++ backend/SmartAccess/SmartAccess.API/Program.cs
@@ -1,30 +1,65 @@
+using Scalar.AspNetCore;
 using SmartAccess.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Servicios
+
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
+
+// CORS
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:4200",
+                "https://localhost:4200"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
 // Inyección de Dependencias
+
 builder.Services.AddSingleton<FirebaseService>();
 builder.Services.AddScoped<AuthService>();
+
 builder.Services.AddLogging();
 
 var app = builder.Build();
 
+
 // Middleware
+
 if (app.Environment.IsDevelopment())
 {
+    // OpenAPI (.NET)
+    app.MapOpenApi();
+
+    // Scalar
+    app.MapScalarApiReference();
+
+    // Swagger
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("FrontendPolicy");
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
