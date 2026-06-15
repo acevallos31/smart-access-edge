9a53fba37df69ab8f1899650478882fec8b224d5
Axeell Cevallos <acevallos31@gmail.com>
2026-06-15 13:57:16 -0600

    Correccion de Register Dto

--- backend/SmartAccess/SmartAccess.API/DTOs/RegisterDto.cs
+++ backend/SmartAccess/SmartAccess.API/DTOs/RegisterDto.cs
@@ -6,9 +6,9 @@
         // lo que el fontend manda cuando alguien se quiere registrar
 
         public string FullName { get; set; } = string.Empty;
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
     }
 }
