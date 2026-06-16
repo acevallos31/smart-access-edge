69f1b614bdef729e4b927f6707db610ccae656ea
Axeell Cevallos <acevallos31@gmail.com>
2026-06-15 19:31:01 -0600

    Agredao Controlador ReportsController correcto

--- backend/SmartAccess/SmartAccess.API/Controllers/ReportsController.cs
+++ backend/SmartAccess/SmartAccess.API/Controllers/ReportsController.cs
@@ -1,6 +1,18 @@
-﻿namespace SmartAccess.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/reports")]
+public class ReportsController : ControllerBase
 {
-    public class Class
+    [HttpGet("statistics")]
+    public IActionResult GetStatistics()
     {
+        return Ok(new
+        {
+            empleadosActivos = 0,
+            presentesHoy = 0,
+            tardanzasHoy = 0,
+            ausenciasHoy = 0
+        });
     }
 }
